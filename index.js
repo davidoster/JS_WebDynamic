511b62f8a91b1cf19895760d83242f25113ba5df
David Oster aka George Pasparakis <paspa@hotmail.com>
2021-06-10 19:54:16 +0300

    login form
    with methods:
    - GET
    - POST

--- index.js
+++ index.js
@@ -11,14 +11,19 @@ const myHTML = `<!DOCTYPE html>
     <title>Document</title>
 </head>
 <body>
-    <p>Hello World of ${course}</p>
+    <p>Hello World</p>
+    <form action="http://localhost:3001/login" method="POST">
+        Username: <input type="text" name="username"><br>
+        Password: <input type="password" name="password">
+        <input type="submit"  value="Login">
+    </form>
 </body>
 </html>`
 
 
 app.get(['/','/index.html'], (req, res) => { // req = request, res = response
     console.log(req.hostname, req.port, req.ip, req.method, req.originalUrl, req.path, req.url)
-    res.send(myHTML)
+    res.send("Hello")
 })
 
 app.get('/mytest', (req, res) => { // HTTP GET
@@ -33,6 +38,19 @@ app.get('/test2.html', (req,res) => {
     // res.redirect('https://in.gr') // redirect to external url
     res.redirect(`http://localhost:${port}/tags/psifiako-pistopoiitiko/`) // redirect to internal url
 })
+
+// show the login page
+app.get('/login', (req, res) => { 
+    res.send(myHTML)
+})   
+
+// show the result from the login process
+app.post('/login', (req, res) => { 
+    console.log(req.hostname, req.port, req.ip, req.method, req.originalUrl, req.path, req.url)
+    // if(req.params) // username == 'admin' && password == 'admin'
+    res.send('Came from login')
+})
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
